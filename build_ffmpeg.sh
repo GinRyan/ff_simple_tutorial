9491a2fa76a6e2546916856dfdafddf5aad732a6
Xelloss <lp8800lz@gmail.com>
2017-06-24 12:29:07 +0800

    add download batch command

--- build_ffmpeg.sh
+++ build_ffmpeg.sh
@@ -1,6 +1,26 @@
-export PREFIX=`pwd`/../ffmpeg-dev
+#!/bin/sh
+export FFMPEG_VERSION="3.3.2"
+export FFMPEG_SRC_DIR="ffmpeg-$FFMPEG_VERSION"
+export FFMPEG_SRC_ARC_FILENAME="$FFMPEG_SRC_DIR.tar.bz2"
+export DOWNLOAD_URL="http://ffmpeg.org/releases/$FFMPEG_SRC_ARC_FILENAME"
 
-./configure --prefix=$PREFIX --disable-gpl --enable-static --enable-shared --enable-pic 
+if [ ! -f "$FFMPEG_SRC_ARC_FILENAME" ]; then
+  wget $DOWNLOAD_URL
+fi
+
+if [ ! -d "$FFMPEG_SRC_DIR" ]; then
+  tar -jxvf $FFMPEG_SRC_ARC_FILENAME
+fi
+
+cd $FFMPEG_SRC_DIR
+export OUT_PREFIX=`pwd`/../ffmpeg-dev
+
+./configure --prefix=$OUT_PREFIX \
+ 	--disable-gpl \
+	--enable-static \
+	--enable-shared \
+	--enable-pic \
 
 make -j`cat /proc/cpuinfo|grep processor|wc -l` & make install
-
+cd ..
+rm -rf $FFMPEG_SRC_DIR
--- build_sdl.sh
+++ build_sdl.sh
@@ -1,5 +1,24 @@
-export PREFIX=`pwd`/../SDL-dev
+#!/bin/sh
+export SDL_VER="2.0.5"
+export SDL_SRC_DIR="SDL2-$SDL_VER"
+export SDL_SRC_ARC_FILE="$SDL_SRC_DIR.tar.gz"
+export DL_URL="http://www.libsdl.org/release/$SDL_SRC_ARC_FILE"
 
-./configure --prefix=$PREFIX  
+if [ ! -f "$SDL_SRC_ARC_FILE" ]; then 
+   wget $DL_URL
+fi
+
+if [ ! -d "$SDL_SRC_DIR" ]; then 
+   tar -xzvf $SDL_SRC_ARC_FILE
+fi
+
+cd $SDL_SRC_DIR
+
+export OUT_PREFIX=`pwd`/../SDL-dev
+
+./configure --prefix=$OUT_PREFIX  
 
 make -j`cat /proc/cpuinfo|grep processor|wc -l` & make install
+
+cd ..
+rm -rf $SDL_SRC_DIR
