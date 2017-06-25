d4397ec7c2263d587d5d4bc28ad27c5c26b26e41
Xelloss <lp8800lz@gmail.com>
2017-06-25 12:43:18 +0800

    complete first demo that encode yuv raw video to H264 single stream video.

--- encodefromyuvtoh264.c
+++ encodefromyuvtoh264.c
@@ -7,6 +7,7 @@
 
 #include "errflags.h"
 
+static int read_yuv_data(int color);
 /**
 * AVCodec 结构保存了一个编解码器的实例。是编解码器。要使用一个指向他的指针。
 * AVCodecContext 代表编解码的上下文，用于储存编解码所需要的参数。也是使用指针保存
@@ -33,7 +34,7 @@ AVCodecContext* codecCtx= NULL;
 //video frame before encoding
 AVFrame* frame = NULL;
 //video and audio packet after encoding.
-AVPacket* pkt;
+AVPacket pkt;
 
 static int parse_input_parameters(int argc,char **argv){
     for (int i = 0;i < argc;i++) {
@@ -68,6 +69,7 @@ static int parse_input_parameters(int argc,char **argv){
 * @return
 */
 int main(int argc,char **argv){
+    int got_packet = 0;
     if(parse_input_parameters(argc,argv) > 0){
         printf("Input File:\t%s\nOutputFile:\t%s\nFrame resolution:\t%d x %d \nbitrate: \t%d\nFrameNumToEncode:\t%d\n",
                inputFileName,outputFileName,
@@ -83,33 +85,43 @@ int main(int argc,char **argv){
     avcodec_register_all();
     //find all needed encoder or decoder.
     //There we use h264 encoder.
+    printf("Init all codec\n");
     codec = avcodec_find_encoder(AV_CODEC_ID_H264);
+    printf("Find codec\n");
     if (!codec) {
+        printf("Find codec error\n");
         return FF_ERROR_INITIAL_FAILED;
     }
 
+    printf("Setting codec context parameters\n");
     //allocate avcodec context instance
     codecCtx =  avcodec_alloc_context3(codec);
     if (!codecCtx) {
+        printf("AV codec context alloc Error\n");
         return  FF_ERROR_INITIAL_FAILED;
     }
     codecCtx->width = frameWidth;
     codecCtx->height = frameHeight;
     codecCtx->bit_rate = bitrate;
-    AVRational r=  {1,25};
-    codecCtx->gop_size = 10;
+
+    codecCtx->time_base = av_make_q(1,25);
+    codecCtx->gop_size = 12;
     codecCtx->max_b_frames = 1;
     codecCtx->pix_fmt = AV_PIX_FMT_YUV420P;
+
     av_opt_set(codecCtx->priv_data,"present","slow",0);
+
     //ok, parameters settings' ok.
     //open encoder.
     int openCodec = avcodec_open2(codecCtx,codec,NULL);
-    if (!openCodec) {
+    if (openCodec<0) {
+        printf("Open codec Error\n");
         return FF_ERROR_INITIAL_FAILED;
     }
     //allocate AVFrame and pixel space.
     frame = av_frame_alloc();
     if (!frame) {
+        printf("AVFrame alloc error\n");
         return FF_ERROR_INITIAL_FAILED;
     }
     frame->width=codecCtx->width;
@@ -117,12 +129,72 @@ int main(int argc,char **argv){
     frame->format=codecCtx->pix_fmt;
     int localAv_image_alloc = av_image_alloc(frame->data,frame->linesize,frame->width,frame->height,frame->format,32);
     if (!localAv_image_alloc) {
+        printf("AVImage alloc error\n");
         return FF_ERROR_INITIAL_FAILED;
     }
+
+    printf("start encode loop\n");
+    //start encode loop
+    for (int frameIdx = 0;frameIdx < frameToEncode;frameIdx++) {
+        av_init_packet(&pkt);
+        pkt.data = NULL;
+        pkt.size = 0;
+
+        //read data to AVFrame
+        read_yuv_data(0);
+        read_yuv_data(1);
+        read_yuv_data(2);
+        frame->pts = frameIdx;
+
+        if (avcodec_encode_video2(codecCtx,&pkt,frame,&got_packet)<0) {
+            printf("Error: Encoding failed!\n");
+            return  FF_ERROR_ENCODING_FAILED;
+        }
+        if(got_packet){
+            printf("write packet to frame:%d, size=%d \n",frameIdx,pkt.size);
+            fwrite(pkt.data,1,(unsigned long)pkt.size,pFout);
+            av_packet_unref(&pkt);
+        }
+    }
+    for (got_packet=1;got_packet;) {
+        if (avcodec_encode_video2(codecCtx,&pkt,NULL,&got_packet)<0) {
+            printf("Error: Encoding failed!\n");
+            return  FF_ERROR_ENCODING_FAILED;
+        }
+        if(got_packet){
+            printf("write cached packet size=%d \n",pkt.size);
+            fwrite(pkt.data,1,(unsigned long)pkt.size,pFout);
+            av_packet_unref(&pkt);
+        }
+    }
+    //close file
+    fclose(pFin);
+    fclose(pFout);
+    avcodec_close(codecCtx);
+    av_free(codecCtx);
+    av_freep(frame->data);
+    av_frame_free(&frame);
+    printf("Bye!\n");
     return EXIT_SUCCESS;
 }
 
-
+static int read_yuv_data(int color){
+    //color = 0 is Y
+    //color = 1 is U
+    //color = 2 is V
+    int color_height = color == 0 ? frameHeight: frameHeight / 2;
+    int color_width = color == 0 ? frameWidth:frameWidth / 2;
+    int color_size = color_height * color_width;
+    int color_stride = frame->linesize[color];
+    if (color_width == color_stride) {
+        fread(frame->data[color],1,(unsigned long)color_size,pFin);
+    } else {
+        for (int rowidx = 0;rowidx < color_height;rowidx++) {
+            fread(frame->data[color] + rowidx * color_stride,1,(unsigned long)color_width,pFin);
+        }
+    }
+    return color_size;
+}
 
 
 
